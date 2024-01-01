1340b75000e0af572e34a0b1149528452f4052c1
swayambansal01@gmail.com <swayambansal01@gmail.com>
2024-01-01 12:14:12 -0500

    added text

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ pygame.init()
 width = 800
 height = 400
 screen = pygame.display.set_mode((width, height))
+screen.fill('White')
 
 # adding a title for the game, screen ...
 pygame.display.set_caption('Runner')
@@ -17,10 +18,18 @@ pygame.display.set_caption('Runner')
 # creating a clock, for the framerates and other niche things
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill('Red')
+# creating a font 
+test_font = pygame.font.Font(None, 35)
 
-#setting up the game loop
+# creating surface for the images 
+ground_surface = pygame.image.load('Images/GroundImage.png') 
+cloud_surface = pygame.image.load('Images/Clouds.png')
+
+# creating a surface for the font 
+text_surface = test_font.render('score', 0, 'Grey30')
+
+
+# setting up the game loop
 while True:
     # checks for any and all events occuring in the loop ...
     for event in pygame.event.get():
@@ -36,8 +45,19 @@ while True:
                 # allows to shut down the code itself 
 
     # blit is used to display a screen on top of other screen.
-    screen.blit(test_surface, (350,100))
+    # we use it to add images on the display screen 
+    # displaying the ground ...
+    screen.blit(ground_surface, (0,300))
 
+    # display the score ...
+    screen.blit(text_surface, (700, 10))
+
+    # displaying the clouds ...  
+    screen.blit(cloud_surface, (60,53))
+    screen.blit(cloud_surface, (533,141))
+
+    # always at the end of the game loop
+    # updates the display ... 
     pygame.display.update()
     
     # adding the celling to the fps of the game at 60 fps
