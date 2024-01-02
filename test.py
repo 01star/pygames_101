af8414a8d0ad33da8b13ec51c841bc8a2c493fee
swayambansal01@gmail.com <swayambansal01@gmail.com>
2024-01-01 19:20:25 -0500

    added game states

--- test.py
+++ test.py
@@ -12,6 +12,9 @@ height = 400
 screen = pygame.display.set_mode((width, height))
 screen.fill('White')
 
+# the game state flag 
+game_active = True
+
 # adding a title for the game, screen ...
 pygame.display.set_caption('Runner')
 
@@ -28,7 +31,7 @@ background_surface = pygame.image.load('Images/background.png').convert_alpha()
 ground_surface = pygame.image.load('Images/GroundImage.png').convert_alpha()
 cloud_surface = pygame.image.load('Images/Clouds.png').convert_alpha()
 
-cactus_surface = pygame.image.load('Images/Cactus/Cactus2.png').convert_alpha()
+cactus_surface = pygame.image.load('Images/Cactus/Cactus1.png').convert_alpha()
 cactus_rectangle = cactus_surface.get_rect(midbottom = (600, 310))
 
 player_surface = pygame.image.load('Images/Dino/Dino_standing.png')
@@ -56,55 +59,63 @@ while True:
                 # allows to shut down the code itself 
             
         # key events --
-        if event.type == pygame.KEYDOWN:
+        # checking for the game state, and sort of making the game resart 
+        if event.type == pygame.KEYDOWN and game_active == True:
             if event.key == pygame.K_SPACE:
                 if (player_rectangle.bottom == player_floor):
                     player_gravity = -20
-                # player_gravity = -20
+
+        elif event.type == pygame.KEYDOWN and game_active == False:
+            game_active = True
+            cactus_rectangle = cactus_surface.get_rect(midbottom = (600, 310))
+            player_rectangle = player_surface.get_rect(midbottom = (100, player_floor))
 
         # mouse event --
         # if event.type == pygame.MOUSEBUTTONDOWN:
         #     if player_rectangle.collidepoint(event.pos):
         #         print ('mosue collision')
 
-    # blit is used to display a screen on top of other screen.
-    # we use it to add images on the display screen 
+    if game_active:
+        # blit is used to display a screen on top of other screen.
+        # we use it to add images on the display screen 
 
-    # creating the background ..
-    screen.blit(background_surface, (0,0))
+        # creating the background ..
+        screen.blit(background_surface, (0,0))
 
-     # displaying the ground ...
-    screen.blit(ground_surface, (0,300))
+        # displaying the ground ...
+        screen.blit(ground_surface, (0,300))
 
-    # displaying the cactus ...
-    screen.blit(cactus_surface, cactus_rectangle)
-    cactus_rectangle.left -= 3
-    if (cactus_rectangle.left < -100):
-        cactus_rectangle.left = width + 100
+        # displaying the cactus ...
+        screen.blit(cactus_surface, cactus_rectangle)
+        cactus_rectangle.left -= 3
+        if (cactus_rectangle.left < -100):
+            cactus_rectangle.left = width + 100
 
-    # display the score ...
-    screen.blit(score_surface, (650, 20))
+        # display the score ...
+        screen.blit(score_surface, (650, 20))
 
-    # displaying the clouds ...  
-    screen.blit(cloud_surface, (60,53))
-    screen.blit(cloud_surface, (533,141))
+        # displaying the clouds ...  
+        screen.blit(cloud_surface, (60,53))
+        screen.blit(cloud_surface, (533,141))
 
-    # Player 
-    # displaying the character (dino)
-    screen.blit(player_surface, player_rectangle)
-    player_gravity += 1                         # increase the gravity non-lienarly
-    player_rectangle.bottom += player_gravity   # make player fall with that gravity
-  
-    if (player_rectangle.bottom >= player_floor):
-        # if the player is going below the surface
-        player_gravity = 0                      # set gravity back to zero
-        player_rectangle.bottom = player_floor  # set the player at the floor
+        # Player 
+        # displaying the character (dino)
+        screen.blit(player_surface, player_rectangle)
+        player_gravity += 1                         # increase the gravity non-lienarly
+        player_rectangle.bottom += player_gravity   # make player fall with that gravity
     
+        if (player_rectangle.bottom >= player_floor):
+            # if the player is going below the surface
+            player_gravity = 0                      # set gravity back to zero
+            player_rectangle.bottom = player_floor  # set the player at the floor
         
-    # checking for the collisions
-    if player_rectangle.colliderect(cactus_rectangle):
-        pygame.quit() 
-        exit()
+            
+        # checking for the collisions
+        if player_rectangle.colliderect(cactus_rectangle):
+            game_active = False
+
+    else:
+        screen.fill('white')
 
     # always at the end of the game loop
     # updates the display ... 
